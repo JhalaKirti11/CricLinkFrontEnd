cf75430a8ebb108c914eea1773b76c74b29abd6b
Kirti11 <kirtijhala1110@gmail.com>
2025-04-10 23:24:02 +0530

    Update team.js

--- src/Components/Team/team.js
+++ src/Components/Team/team.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import url from "../../URL/url.js";
 import Footer from '../Footer.js';
-
-import './team.css'; // Import your CSS file
+import './team.css';
 import { useNavigate } from "react-router-dom";
 
 export default () => {
@@ -39,7 +38,6 @@ export default () => {
                                 <img src={team.thumbnail} className="card-img-top" height='200px' alt='TeamImage' />
                                 <div className="card-body">
                                     <h5 className="card-title mt-2 text-light">{team.team_name}</h5>
-                                    {/* <p className="card-text mt-2">{team.captain_name}</p> */}
                                 </div>
                                 <button className="btn btn-secondary" onClick={() => navigate('/teamDetails', { state: team })} id="teamViewMore">View More</button>
                             </div>
