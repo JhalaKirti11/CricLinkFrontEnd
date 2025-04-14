ad851d592257997360f319d3ec47750c1ce157ce
Kirti11 <kirtijhala1110@gmail.com>
2025-04-14 23:47:25 +0530

    Update About.js

--- src/Components/HomePage/About.js
+++ src/Components/HomePage/About.js
@@ -1,31 +1,5 @@
-// import './homePage.css';
-// //     width: 98.6vw;
-// export default function About() {
-//     return <>
-//         <div id="AboutContainer">
-//             <div className='text-center text-decoration-underline'><h2>ABOUT US</h2></div>
-//             <div className='mt-4' id="maintext">
-//                 <div id="imgDiv" className='position-relative'>
-//                     {/* <img src="assets/cricket.webp" id="aboutImg" /> */}
-//                     {/* <img src="https://media.istockphoto.com/id/518022060/photo/cricket-batsman-hitting-ball-during-cricket-match-in-stadium.webp?b=1&s=170667a&w=0&k=20&c=qsbdTpZ2wcOJwcdNXPeErUzS-1QKZuunuv3GGgP6dKA=" alt='About image' className='img-fluid' id="aboutImg" /> */}
-//                     <div id="textDiv" className='text-center p-3 rounded'>
-//                         <h3 id="h3">Cricket Platform</h3>
-//                         <div id="text" className='rounded p-3'>
-//                             <p id="p">
-//                                 In this platform, we provide an interface connecting users with event organizers. It's the ideal platform for registering events and participating in them. Our application offers an easy way to create teams and take part in cricket tournaments.
-//                             </p>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     </>
-// }
-
-
-
 import React from 'react';
-import './AboutUs.css'; // Make sure to create a CSS file for styling
+import './AboutUs.css';
 
 const AboutUs = () => {
   return (
@@ -151,7 +125,6 @@ const AboutUs = () => {
           </div>
         </div>
         
-//////////////////////
         <div className="map-container">
         <h2>Our Location</h2>
         <iframe 
@@ -164,7 +137,6 @@ const AboutUs = () => {
           allowFullScreen
         ></iframe>
       </div>
-      //////////////////////////
       <div className="video-container">
         <h2>Watch Our Story</h2>
         <iframe 
@@ -177,8 +149,8 @@ const AboutUs = () => {
           allowFullScreen
         ></iframe>
       </div>
-//////////
-<div className="ending-section">
+
+      <div className="ending-section">
         <h2 className="ending-title">Thank You for Visiting!</h2>
         <p className="ending-description">
           We hope you enjoyed learning about cricket. Stay tuned for more exciting updates and news from the world of cricket.
