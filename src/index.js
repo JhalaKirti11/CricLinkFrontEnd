f38babab4de7e8fbca2ede010b14d92c3feecffe
Kirti11 <kirtijhala1110@gmail.com>
2025-04-15 22:01:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,14 +10,6 @@ import store from './redux-config/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  {/* <Auth0Provider
-    domain="dev-zcj5rouifmqfpc7l.us.auth0.com"
-    clientId="EVw6jxWTWwWLkZgJSyDzUj8aIqSqHJY0"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}>
-    <App />
-  </Auth0Provider>, */}
     <GoogleOAuthProvider clientId='771317770066-mumcjnlkakmj3t8osvgmj8mur30sttfi.apps.googleusercontent.com'>
       <Provider store={store}>
         <App />
