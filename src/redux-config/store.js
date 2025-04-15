c8d78b12568f5c9daa6de008fa93175dc0e64975
Kirti11 <kirtijhala1110@gmail.com>
2025-04-15 21:59:34 +0530

    Update store.js

--- src/redux-config/store.js
+++ src/redux-config/store.js
@@ -1,29 +1,3 @@
-// // store.js
-// import { createSlice } from '@reduxjs/toolkit';
-// import axios from 'axios';
-// import url from '../URL/url';
-
-// const store = createSlice({
-//     name: 'FormData',
-//     initialState: {
-//         teamName: '',   
-//         totalPlayers: '',
-//         batsmen: '',
-//         bowlers: '',
-//         allRounders: '',
-//         wicketKeepers: '',
-//     },
-//     reducers: {
-//         addTeam: (state, action) => {
-//             axios.post(url.team.register, state)
-//                 .then()
-//                 .catch();
-//         }
-//     }
-// });
-
-// export default store;
-
 import { configureStore } from "@reduxjs/toolkit";
 import CategorySlice from "./CategorySlice.js";
 import SubCategorySlice from "./SubCategorySlice.js";
