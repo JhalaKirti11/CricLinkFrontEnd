781aaa0e02d0247a55cd9567a4db2a6a7bec4f54
Kirti11 <kirtijhala1110@gmail.com>
2025-04-17 23:32:56 +0530

    Update closeForm.js

--- src/Components/Player/closeForm.js
+++ src/Components/Player/closeForm.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import UpdateProfileForm from './UpdateProfileForm'; // UpdateProfileForm ko import karein
+import UpdateProfileForm from './UpdateProfileForm';
 
 const ParentComponent = () => {
     const [showForm, setShowForm] = useState(false);
-
-    // Close form function
     const closeForm = (value) => {
         setShowForm(value);
     };
@@ -12,10 +10,8 @@ const ParentComponent = () => {
     return (
         <div>
             <button onClick={() => setShowForm(true)}>Update Profile</button>
-
             {showForm && <UpdateProfileForm closeForm={closeForm} />}
         </div>
     );
 };
-
 export default ParentComponent;
